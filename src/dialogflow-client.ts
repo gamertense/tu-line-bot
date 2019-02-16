839685595522c22ef1627b07f212f6e8e1f67ca6
Antonio Jack <ict2jack@gmail.com>
2019-02-16 20:37:59 +0700

    JSON.stringify

--- src/dialogflow-client.ts
+++ src/dialogflow-client.ts
@@ -91,7 +91,7 @@ export class DialogflowClient {
     let i = 0;
     // for (let i = 0; i < dialogflowMessages.length; i++) {
     const messageType = get(dialogflowMessages[i], 'message');
-    console.log(`Dialogflow ${dialogflowMessages[i]}`)
+    console.log(`Dialogflow ${JSON.stringify(dialogflowMessages[i])}`)
     let message: Message;
     if (messageType === 'text') {
       message = {
