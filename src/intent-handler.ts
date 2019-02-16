f6b606fa734f836451ba8a88f1ae23da624862fd
Antonio Jack <ict2jack@gmail.com>
2019-02-16 23:50:25 +0700

    Add index of array.

--- src/intent-handler.ts
+++ src/intent-handler.ts
@@ -14,8 +14,8 @@ export class IntentHandler {
         switch (this.intentName) {
             case 'voterest - custom - yes':
                 this.isIntentMatch = true;
-                const rest_name = get(result, ['outputContexts', 'parameters', 'rest_name']);
-                const point = get(result, ['outputContexts', 'parameters', 'point']);
+                const rest_name = get(result, ['outputContexts', '0', 'parameters', 'rest_name']);
+                const point = get(result, ['outputContexts', '0', 'parameters', 'point']);
                 console.log(rest_name, point)
                 let message: Message;
 
